5fbcf2bfb877663b75ae682bd7ae5202ac97000f
Rahul Kadyan <hi@znck.me>
2017-11-26 12:41:30 +0530

    fix: install pnpm using curl

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM node:latest
 
-RUN npm --silent install --ignore-scripts --global --depth 0 pnpm
-
-RUN mkdir /root/.pnpm-store && chmod 777 /root/.pnpm-store
+RUN curl -sL https://unpkg.com/@pnpm/self-installer | node
--- hooks/pre_build.sh
+++ hooks/pre_build.sh
@@ -3,7 +3,5 @@
 cat > Dockerfile << EOF
 FROM node:${CACHE_TAG:-latest}
 
-RUN npm --silent install --ignore-scripts --global --depth 0 pnpm
-
-RUN mkdir /root/.pnpm-store && chmod 777 /root/.pnpm-store
+RUN curl -sL https://unpkg.com/@pnpm/self-installer | node
 EOF
