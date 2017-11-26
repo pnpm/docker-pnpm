a5a8980591d01edb4b3aad44f182b1e29b16084b
Rahul Kadyan <hi@znck.me>
2017-11-26 09:03:22 +0530

    fix: add pnpm-store with correct permission

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM node:latest
 
 RUN npm --silent install --ignore-scripts --global --depth 0 pnpm
+
+RUN mkdir /root/.pnpm-store && chmod 777 /root/.pnpm-store
--- hooks/pre_build.sh
+++ hooks/pre_build.sh
@@ -4,4 +4,6 @@ cat > Dockerfile << EOF
 FROM node:${CACHE_TAG:-latest}
 
 RUN npm --silent install --ignore-scripts --global --depth 0 pnpm
+
+RUN mkdir /root/.pnpm-store && chmod 777 /root/.pnpm-store
 EOF
